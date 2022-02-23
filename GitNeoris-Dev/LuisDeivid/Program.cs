3475bc60c47f5347d3ac289d460b51064d58860a
Llanes520 <95722832+Llanes520@users.noreply.github.com>
2022-02-23 11:09:48 -0500

    Metodo Deivid

---
+++ GitNeoris-Dev/LuisDeivid/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LuisDeivid
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.MetodoLuis();
+            program.MetodoDeivid();
+        }
+
+        public void MetodoLuis()
+        {
+            Console.WriteLine("Este es el metodo de luis");
+        }
+        public void MetodoDeivid()
+        {
+            Console.WriteLine("Este es el metodo de Deivid");
+        }
+    }
+}
