5c3e9872cfe002e3c9cab6f0483e101b3e39e730
luisado123 <dro783249@gmail.com>
2022-02-23 08:24:58 -0500

    Add project files.

---
+++ LuisDeivid/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LuisDeivid
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.MetodoLuis();
+        }
+
+        public void MetodoLuis()
+        {
+            Console.WriteLine("Este es el metodo de luis");
+        }
+    }
+}
